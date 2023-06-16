ab7a6a6e3bcccf41259a639b250072f8510140a3
mishra-ashish-bdm <ashish@bdmdigitech.com>
2023-06-16 18:51:20 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,80 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import userRouter from './routes/userRoute.js'
-import rentalRouter from './routes/rentalRoute.js'
-import vehicleRouter from './routes/vehicleRoute.js'
-const app = express()
-app.use(express.json())
-dotenv.config()
-connectDB()
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRouter from './routes/userRoute.js';
+import rentalRouter from './routes/rentalRoute.js';
+import vehicleRouter from './routes/vehicleRoute.js';
+import seedData from './seeder/vehicleSeed.js';
+import fs from 'fs';
 
-let PORT = process.env.PORT || 3000
+const app = express();
+app.use(express.json());
+dotenv.config();
+connectDB();
 
-app.use(userRouter)
-app.use(vehicleRouter)
-app.use(rentalRouter)
+let PORT = process.env.PORT || 3000;
 
+// Function to check if the seeded data flag file exists
+async function checkSeededDataFlag() {
+    return new Promise((resolve) => {
+        fs.access('./seededDataFlag', fs.constants.F_OK, (err) => {
+            if (err) {
+                // Flag file does not exist
+                resolve(false);
+            } else {
+                // Flag file exists
+                resolve(true);
+            }
+        });
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-})
+// Function to create the seeded data flag file
+async function setSeededDataFlag() {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./seededDataFlag', '', (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+app.use(userRouter);
+app.use(vehicleRouter);
+app.use(rentalRouter);
+
+// Check if the seeded data flag is set
+checkSeededDataFlag()
+    .then((hasSeededData) => {
+        if (!hasSeededData) {
+            seedData()
+                .then(() => {
+                    console.log('Data seeded successfully.');
+                    // Set the seeded data flag
+                    setSeededDataFlag()
+                        .then(() => {
+                            // Start the server
+                            app.listen(PORT, () => {
+                                console.log(`Server is running on ${PORT}`);
+                            });
+                        })
+                        .catch((error) => {
+                            console.error('Error occurred while setting the seeded data flag:', error);
+                        });
+                })
+                .catch((error) => {
+                    console.error('Error occurred while seeding data:', error);
+                });
+        } else {
+            // Start the server directly if data has already been seeded
+            app.listen(PORT, () => {
+                console.log(`Server is running on ${PORT}`);
+            });
+        }
+    })
+    .catch((error) => {
+        console.error('Error occurred while checking the seeded data flag:', error);
+    });
---
+++ src/seeder/vehicleSeed.js
@@ -0,0 +1,45 @@
+import vehicleModel from "../models/vehicleModel.js";
+import mongoose from 'mongoose';
+
+export default async function seedData() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/rentVehicle", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connected to MongoDB server.");
+
+    const seedData = [
+      { type: 'hatchback', name: 'Car1' },
+      { type: 'hatchback', name: 'Car2' },
+      { type: 'suv', name: 'Car3' },
+      { type: 'suv', name: 'Car4' },
+      { type: 'sedan', name: 'Car5' },
+      { type: 'sedan', name: 'Car6' },
+      { type: 'cruiser', name: 'Bike1' },
+      { type: 'sports', name: 'Bike2' }
+    ];
+
+    const savePromises = seedData.map(async (data, index) => {
+      const p = new vehicleModel(data);
+      try {
+        const result = await p.save();
+        if (index === seedData.length - 1) {
+          console.log("DONE!");
+        }
+        return result;
+      } catch (error) {
+        throw error;
+      }
+    });
+
+    await Promise.all(savePromises);
+    console.log("All vehicles saved successfully.");
+  } catch (error) {
+    console.error("Error occurred while saving vehicles:", error);
+  } finally {
+    mongoose.disconnect();
+    console.log("Disconnected from MongoDB server.");
+  }
+}
